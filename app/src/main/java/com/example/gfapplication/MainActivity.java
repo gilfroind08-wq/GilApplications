5bcc3251057406202a805a7bd5f998462f25bd97
gilfroind08-wq <gilfroind08@gmail.com>
2025-11-05 14:57:56 +0200

    עשינו סיקבאר ומתג

--- app/src/main/java/com/example/gfapplication/MainActivity.java
+++ app/src/main/java/com/example/gfapplication/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.gfapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-     Button openSecondButton = findViewById(R.id.openSecondButton);
+     SeekBar seekBar;
+     Switch switch2;
     private ImageView myImage;
     private Button toggleButton, darkButton, brightButton;
     private boolean isImageVisible = true;
@@ -19,12 +26,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+            SeekBar seekBar = findViewById(R.id.seekBar);
             myImage = findViewById(R.id.myImage);
             toggleButton = findViewById(R.id.toggleButton);
             darkButton = findViewById(R.id.darkButton);
             brightButton = findViewById(R.id.brightButton);
-
+            switch2 = findViewById(R.id.switch2);
 
         // כפתור הצגה / הסתרה
         toggleButton.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +46,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // כפתור פחות בהירות
+
+
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b == true) {
+          ;          myImage.setVisibility(View.INVISIBLE);
+                } else {
+                    myImage.setVisibility(View.VISIBLE);
+                }
+                isImageVisible = !isImageVisible;
+            }
+
+        });{
+
+seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        float alpha = i / 100f; //
+        myImage.setAlpha(alpha);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        Toast.makeText(MainActivity.this, "נלחץ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        Toast.makeText(MainActivity.this, "שוחרר", Toast.LENGTH_SHORT).show();
+    }
+});
+                                           }
+
+                // כפתור פחות בהירות
         darkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
